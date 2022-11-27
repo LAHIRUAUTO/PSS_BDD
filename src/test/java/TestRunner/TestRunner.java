236cc61a9a337e9de17b909619df2c0e9877aa9c
LAHIRU <Vikasithasouth@gmail.com>
2022-11-27 21:10:17 +0530

    log4j issue fixed and Dryrun attribute added

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -14,6 +14,7 @@ import io.cucumber.testng.CucumberOptions;
                 "json:reporting/cucumber.json"
         },
         monochrome = true,
+        dryRun = false,
         tags = ("@SmokeTest")
 
 )
